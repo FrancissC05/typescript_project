56e6ab177aeaa42ae57da5386ed92758a6a1598c
FrancissC05 <dev.fcartagena@gmail.com>
2026-01-10 00:27:41 -0500

    refactor text event file

--- src/domain/events.ts
+++ src/domain/events.ts
@@ -1,7 +1,15 @@
-export interface IncomingTextEvent {
-    kind: "text";
-    messageId: string;
-    from: string;
-    body: string;
-    phoneNumberId: string;
-}
+export type IncomingTextEvent =
+    | {
+        kind: "text";
+        messageId: string;
+        from: string;
+        body: string;
+        phoneNumberId: string;
+    }
+    | {
+        kind: "button";
+        messageId: string;
+        from: string;
+        buttonId: string;
+        phoneNumberId: string;
+    };
